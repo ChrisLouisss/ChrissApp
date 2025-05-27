06614f216d9f8a54e8dc5020778be9df4e262d27
csyhlim <caseyhlimx@gmail.com>
2025-05-27 10:17:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chrisapp/ChrisActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chrisapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChrisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chris)
+    }
+}
